455fa0f4d0ebbb57532cc2645b17a07421bdc9c2
sprakas2 <sprakas2@uncc.edu>
2017-09-22 04:32:41 -0400

    first one

---
+++ README.md
@@ -0,0 +1,27 @@
+# [Red Ventures - Pool Pros]
+
+It is a semi- functional website for the users to find and get best in place Pool Services and Installation. 
+
+## About
+
+- It starts with a static display of few dealer records(just for the purpose of filling the body with some content initially).
+
+- When the used clicks on "Find a Pro" button , given json data is parsed and asynchronously displayed on the home page. 
+  -- since the data available for just one zipcode, temporarily we are not showing the results based on user location
+  
+  -- However, I have also included an option to retrieve the current zip code of the user on click of the button.(For future use)
+  
+- Modal boxes are designed for users to key in their contact information along with their queries. 
+
+- basic validation is done. 
+
+## Future Extentions
+
+- Filter feature can be implemented
+- Can display dealers available closer to the user location. 
+- chat system can be integrated for immediate assistance.
+
+## Execution
+
+Run the index.html
+
---
+++ js/pool.js
@@ -0,0 +1,364 @@
+var dealersObj = {
+    "zipcode": "28205",
+    "location": {
+        "lat": 35.2263714,
+        "lon": -80.7990185
+    },
+    "dealers": [
+        {
+            "data": {
+                "companyID": 401929,
+                "name": "Aqua Experts",
+                "phone1": "1-888-234-5678",
+                "email": "info@aquaexperts.com",
+                "addressLine1": "1234 Some Street",
+                "addressLine2": "",
+                "city": "Charlotte",
+                "state": "North Carolina",
+                "country": "US",
+                "zipcode": "28205",
+                "weekHours": {
+                    "mon": "7:00am - 7:00pm",
+                    "tue": "7:00am - 7:00pm",
+                    "wed": "7:00am - 7:00pm",
+                    "thu": "7:00am - 7:00pm",
+                    "fri": "7:00am - 7:00pm",
+                    "sat": "7:00am - 3:00pm",
+                    "sun": ""
+                },
+                "certifications": [
+                    "Installation Pro",
+                    "Residential Pro",
+                    "Service Pro"
+                ]
+            }
+        }, {
+            "data": {
+                "companyID": 401928,
+                "name": "Premium Pools & Spas of Charlotte",
+                "phone1": "1-888-888-8888",
+                "email": "info@premiumpoolscharlotte.com",
+                "addressLine1": "1234 Some Street",
+                "addressLine2": "",
+                "city": "Charlotte",
+                "state": "North Carolina",
+                "country": "US",
+                "zipcode": "28205",
+                "weekHours": {
+                    "mon": "7:00am - 7:00pm",
+                    "tue": "7:00am - 7:00pm",
+                    "wed": "7:00am - 7:00pm",
+                    "thu": "7:00am - 7:00pm",
+                    "fri": "7:00am - 7:00pm",
+                    "sat": "",
+                    "sun": "On Call"
+                },
+                "certifications": [
+                    "Installation Pro",
+                    "Commercial Pro",
+                    "Residential Pro",
+                    "Service Pro"
+                ]
+            }
+        }, {
+            "data": {
+                "companyID": 401927,
+                "name": "Carolina Pool Specialists",
+                "phone1": "1-877-987-6543",
+                "email": "info@cps.com",
+                "addressLine1": "1234 Some Street",
+                "addressLine2": "",
+                "city": "Charlotte",
+                "state": "North Carolina",
+                "country": "US",
+                "zipcode": "28205",
+                "weekHours": {
+                    "mon": "8:00am - 5:00pm",
+                    "tue": "8:00am - 5:00pm",
+                    "wed": "8:00am - 5:00pm",
+                    "thu": "8:00am - 5:00pm",
+                    "fri": "8:00am - 5:00pm",
+                    "sat": "8:00am - 5:00pm",
+                    "sun": ""
+                },
+                "certifications": [
+                    "Installation Pro",
+                    "Commercial Pro",
+                    "Residential Pro",
+                    "Service Pro"
+                ]
+            }
+        }, {
+            "data": {
+                "companyID": 401926,
+                "name": "Pools'r'Us",
+                "phone1": "1-888-346-4574",
+                "email": "info@poolsrus.com",
+                "addressLine1": "1234 Some Street",
+                "addressLine2": "",
+                "city": "Charlotte",
+                "state": "North Carolina",
+                "country": "US",
+                "zipcode": "28205",
+                "weekHours": {
+                    "mon": "7:00am - 7:00pm",
+                    "tue": "7:00am - 7:00pm",
+                    "wed": "7:00am - 7:00pm",
+                    "thu": "7:00am - 7:00pm",
+                    "fri": "7:00am - 7:00pm",
+                    "sat": "7:00am - 3:00pm",
+                    "sun": "1:00pm - 5:00pm"
+                },
+                "certifications": [
+                    "Installation Pro",
+                    "Commercial Pro",
+                    "Residential Pro"
+                ]
+            }
+        }, {
+            "data": {
+                "companyID": 401925,
+                "name": "Clean Pool Systems",
+                "phone1": "1-888-246-7485",
+                "email": "info@cleanpools.com",
+                "addressLine1": "1234 Some Street",
+                "addressLine2": "",
+                "city": "Charlotte",
+                "state": "North Carolina",
+                "country": "US",
+                "zipcode": "28205",
+                "weekHours": {
+                    "mon": "7:00am - 7:00pm",
+                    "tue": "7:00am - 7:00pm",
+                    "wed": "7:00am - 7:00pm",
+                    "thu": "7:00am - 7:00pm",
+                    "fri": "7:00am - 7:00pm",
+                    "sat": "7:00am - 3:00pm",
+                    "sun": ""
+                },
+                "certifications": [
+                    "Installation Pro",
+                    "Residential Pro"
+                ]
+            }
+        }, {
+            "data": {
+                "companyID": 401924,
+                "name": "Pool Service Pros",
+                "phone1": "1-888-PRO-POOL",
+                "email": "info@propool.com",
+                "addressLine1": "1234 Some Street",
+                "addressLine2": "",
+                "city": "Charlotte",
+                "state": "North Carolina",
+                "country": "US",
+                "zipcode": "28205",
+                "weekHours": {
+                    "mon": "7:00am - 7:00pm",
+                    "tue": "7:00am - 7:00pm",
+                    "wed": "7:00am - 7:00pm",
+                    "thu": "7:00am - 7:00pm",
+                    "fri": "7:00am - 7:00pm",
+                    "sat": "7:00am - 3:00pm",
+                    "sun": ""
+                },
+                "certifications": [
+                    "Installation Pro",
+                    "Commercial Pro",
+                    "Residential Pro",
+                    "Service Pro"
+                ]
+            }
+        }, {
+            "data": {
+                "companyID": 401923,
+                "name": "Talent Pool",
+                "phone1": "1-888-797-3463",
+                "email": "info@talentpool.com",
+                "addressLine1": "1234 Some Street",
+                "addressLine2": "",
+                "city": "Charlotte",
+                "state": "North Carolina",
+                "country": "US",
+                "zipcode": "28205",
+                "weekHours": {
+                    "mon": "7:00am - 7:00pm",
+                    "tue": "7:00am - 7:00pm",
+                    "wed": "7:00am - 7:00pm",
+                    "thu": "7:00am - 7:00pm",
+                    "fri": "7:00am - 7:00pm",
+                    "sat": "7:00am - 7:00pm",
+                    "sun": "7:00am - 7:00pm"
+                },
+                "certifications": [
+                    "Installation Pro",
+                    "Commercial Pro",
+                    "Service Pro"
+                ]
+            }
+        }
+    ]
+};
+
+$(document).ready(function () {
+var zip;
+  $(".hide").hide();
+    $(".btn-find").on("click", function (e) {
+        e.preventDefault();
+        console.log("retrieving the latitude and longitude");
+        navigator.geolocation.getCurrentPosition(success, error);
+
+        function error(error) {
+            console.log("in error");
+            console.log("The message says ' " + error.message + "'");
+        }
+        // success function
+        function success(position) {
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            console.log("latitude is" + lat);
+            var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyCSCdnGsC8eYcTOx5smXAeD_C-v3yvSzmk"
+            try{
+              $.getJSON(url, function (info) {
+                console.log("entered function" + info)
+                console.log("Your zip code is " + info.results[0].address_components[7].long_name);
+                zip = info.results[0].address_components[7].long_name;
+                
+                 $(".dealer-text").html("Dealers found in " +zip );
+                  $(".dealer-text-mob").html("Dealers found in " +zip );
+                  $('.initial-static-row').hide();
+                $(".hide").show();
+            });  
+            }
+            catch(error){
+                $(".hide").html("Please refresh again and try");
+            }
+        }
+    });
+    
+  
+  console.log(dealersObj.zipcode);
+    
+  for(var i=0;i<dealersObj.dealers.length;i++){
+       $(".result-boxes").append('<div class="col-md-4 mb-4"><div class="card  shadow-bg"><div class="row"><div class="col-md-12"><h4 class="dealer-name">'+dealersObj.dealers[i].data.name+'</h4></div></div><div class="row"><div class="col-md-12"><div class="card-body"><hr><div class="create"><img src="images/phone-circle-blue-20.png" style="margin-right: 13px"><span class="phone-no">'+dealersObj.dealers[i].data.phone1+'</span></div><div class="cant-talk"><p>Can’t talk now? Click below to send an email.</p></div> <button type="button" class="btn rounded-rectangle" data-toggle="modal" data-target="#EmailModal"><span class="mail-icon"><img src="images/email%20icon%20rasterized.png" style="margin-right: 10px">Contact this Pro</span></button> <div><p class="business-hr">Business hours</p><ul class="ul-content"><li>Weekdays&nbsp;'+dealersObj.dealers[i].data.weekHours.mon+'</li><li>Saturdays&nbsp;'+dealersObj.dealers[i].data.weekHours.sat+'</li><li>Sundays&nbsp;'+dealersObj.dealers[i].data.weekHours.sun+'</li></ul></div></div></div></div><div class="card-footer"><div class="row"><div class="col-xs-6 left-pad"><span class="final-footer"><img src="images/star.png" class="left-pad">'+dealersObj.dealers[i].data.certifications[0]+'</span></div><div class="col-xs-6 left-pad"><span class="final-footer"><img class="left-pad" src="images/gear.png">'+dealersObj.dealers[i].data.certifications[1]+'</span></div></div><div class="row"><div class="col-xs-6 left-pad"><span class="final-footer"><img class="left-pad" src="images/home.png">'+dealersObj.dealers[i].data.certifications[2]+'</span></div><div class="col-xs-6 left-pad"><span class="final-footer"><img class="left-pad" src="images/users.png">'+dealersObj.dealers[i].data.certifications[3]+'</span></div></div></div></div></div>');
+      
+  }
+    $('.close').click(function(){
+        closeEmailModal();
+    });
+    $('#sendmail').click(function(){
+        var useremail= $('#user-email').val();
+        var userph= $('#user-ph').val();
+        var uname = $('#first-last-name').val();
+        
+        if (useremail == "" || !validateEmail(useremail)) {
+            alert("Please enter valid Email");
+            $('#user-email').focus();
+        }
+        else if (userph == "" || userph.length !== 10 || !validatePhone(userph)) {
+            alert("Please enter valid Number");
+            $('#user-ph').focus();
+        }
+        else if(uname == ""){
+            alert("Please enter your Name");
+            $('#first-last-name').focus;
+        }
+        
+        else{
+       alert("Mail has been sent. We will shorthly get back to you with more details. Thanks!"); 
+            closeEmailModal();
+        $('#EmailModal').modal('toggle');
+        }
+    });
+    
+    $('#sendmailMob').click(function(){
+       var useremail= $('#user-email').val();
+        var userph= $('#user-ph').val();
+        var uname = $('#first-last-name').val();
+        
+        if (useremail == "" || !validateEmail(useremail)) {
+            alert("Please enter valid Email");
+            $('#user-email').focus();
+        }
+        else if (userph == "" || userph.length !== 10 || !validatePhone(userph)) {
+            alert("Please enter valid Number");
+            $('#user-ph').focus();
+        }
+        else if(uname == ""){
+            alert("Please enter your Name");
+            $('#first-last-name').focus;
+        }
+        
+        else{
+       alert("Mail has been sent. We will shorthly get back to you with more details. Thanks!"); 
+            closeEmailModal();
+        $('#EmailModal').modal('toggle');
+        }
+    });
+    
+    $('#first-last-name').change(function(){
+       if($('#first-last-name').val !== ""){
+           $('#chkName').attr("src","images/checked%20field%20vector.png")
+       }
+        else if($('#first-last-name').val == ""){
+            $('#chkName').attr("src","images/empty%20field%20indicator%20vector.png")
+        }
+    });
+
+    $('#user-ph').change(function(){
+       if($('#user-ph').val !== ""){
+           $('#chkNumb').attr("src","images/checked%20field%20vector.png")
+       }
+        else if($('#user-ph').val == ""){
+            $('#chkNumb').attr("src","images/empty%20field%20indicator%20vector.png")
+        }
+    });
+    $('#user-email').change(function(){
+       if($('#user-email').val !== ""){
+           $('#chkEmail').attr("src","images/checked%20field%20vector.png")
+       }
+        else if($('#user-email').val == ""){
+            $('#chkEmail').attr("src","images/empty%20field%20indicator%20vector.png")
+        }
+    });
+    $('#comments').change(function(){
+       if($('#comments').val !== ""){
+           $('#chkComment').attr("src","images/checked%20field%20vector.png")
+       }
+        else if($('#comments').val == ""){
+            $('#chkComment').attr("src","images/empty%20field%20indicator%20vector.png")
+        }
+    });
+    
+    $(".btn-style").click(function () {
+   $(this).toggleClass("white").siblings().removeClass("white");
+});
+    
+    function validateEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+       
+        if (regex.test(email)) {
+            return true;
+            }
+            else {
+            return false;
+            }
+        }
+    
+     function validatePhone(phNumber) {
+     var regex = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+        if (regex.test(phNumber)) {
+            return true;
+            }
+            else {
+            return false;
+            }
+        }
+        function closeEmailModal()
+        {
+        $('#user-email').val == "";
+        $('#user-ph').val == "";
+        $('#comments').val == "";
+        $('#first-last-name').val == "";}
+        
+});
